788cdca5e72a4ce8b958d258d540d2c33400b69e
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-20 10:42:07 +0400

    add filter option

--- script.js
+++ script.js
@@ -12,8 +12,18 @@ async function getData() {
     response.data.forEach((element) => {
       const li = document.createElement("li");
       li.textContent = `${element.name} ${element.surname}`;
+      li.classList.add("li");
 
       ul.appendChild(li);
+      input.addEventListener("keyup", function () {
+        if (this.value === "") {
+          li.classList.remove("active");
+        } else if (element.name.includes(this.value)) {
+          li.classList.add("active");
+        } else {
+          li.classList.remove("active");
+        }
+      });
     });
   } catch (err) {
     console.log(err);
