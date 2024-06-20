46cbaeca813f9ce0e213c22a13a0a3e18a2bb44c
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-20 10:12:54 +0400

    add try catch in my code

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 
 // https://jsonplaceholder.typicode.com/users
 
+// async function getData() {
+//   let response = await axios.get("https://jsonplaceholder.typicode.com/users");
+//   console.log(response.data);
+// }
+
+// getData();
+
 async function getData() {
-  let response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  console.log(response.data);
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 getData();
