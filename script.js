c5540b7d4cecb0be2eb280bc7af6291c39297d92
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-20 10:46:27 +0400

    add trim and toLowerCase methods on this.value and names

--- script.js
+++ script.js
@@ -8,26 +8,31 @@ async function getData() {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
-    console.log(response.data);
-    response.data.forEach((element) => {
-      const li = document.createElement("li");
-      li.textContent = `${element.name} ${element.surname}`;
-      li.classList.add("li");
-
-      ul.appendChild(li);
-      input.addEventListener("keyup", function () {
-        if (this.value === "") {
-          li.classList.remove("active");
-        } else if (element.name.includes(this.value)) {
-          li.classList.add("active");
-        } else {
-          li.classList.remove("active");
-        }
-      });
-    });
+    filterFnc(response);
   } catch (err) {
     console.log(err);
   }
 }
 
 getData();
+
+function filterFnc(response) {
+  response.data.forEach((element) => {
+    const li = document.createElement("li");
+    li.textContent = `${element.name} ${element.surname}`;
+    li.classList.add("li");
+
+    ul.appendChild(li);
+    input.addEventListener("keyup", function () {
+      if (this.value === "") {
+        li.classList.remove("active");
+      } else if (
+        element.name.toLowerCase().includes(this.value.toLowerCase().trim())
+      ) {
+        li.classList.add("active");
+      } else {
+        li.classList.remove("active");
+      }
+    });
+  });
+}
